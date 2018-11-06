3da2f5aad30e83c652520530805159f2d7e92073
vladimir <mr.vladimir.ice@gmail.com>
2018-11-06 13:53:56 +0300

    * stake-unstake methods * add stake to GET API

--- example.js
+++ example.js
@@ -1,16 +1,21 @@
 const { WalletApi } = require('./index');
 
 (async () => {
-  const accountNameFrom = 'vlad';
-  const accountNameTo = 'jane';
-  const privateKey = 'vlad_private_key';
-  const amount = '10';
+  const accountNameFrom = 'accregistrar';
+  const accountNameTo = 'vlad';
+  const privateKey = '5KSSv6acEyUAm4i11Mw3qxkun8t4rTC63kWs4Heh5NVkHsf6gHe';
+  const amount = '100';
   const memo = '';
 
-  const sendTokensResponse = await WalletApi.sendTokens(accountNameFrom, privateKey, accountNameTo, amount, memo);
-  console.dir(sendTokensResponse);
 
-  const accountState = await WalletApi.getAccountState(accountNameFrom);
+
+
+  // const sendTokensResponse = await WalletApi.sendTokens(accountNameFrom, privateKey, accountNameTo, amount, memo);
+  // console.dir(sendTokensResponse);
+
+  // Check unstake - special field to
+
+  const accountState = await WalletApi.getAccountState(accountNameTo);
   console.log(accountState);
 
   const ramToByInBytes = 6000;
