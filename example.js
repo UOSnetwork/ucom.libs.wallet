25c8454627d19d435ac9b2ed7663fc69625b807c
vladimir <mr.vladimir.ice@gmail.com>
2018-11-06 20:13:50 +0300

    * buy-sell ram methods * text decryptors for node only

--- example.js
+++ example.js
@@ -2,20 +2,24 @@ const { WalletApi } = require('./index');
 
 (async () => {
   /**
-
     Get account state:
     * @link WalletApi#getAccountState
 
     Send tokens:
     * @link WalletApi#sendTokens
 
-    Staking/unstaking form workflow:
+    Staking/unstaking:
     * @link WalletApi#getCurrentNetAndCpuStakedTokens - show in form
     * @link WalletApi#stakeOrUnstakeTokens - send data to this method
 
     Claim emission:
     * @link WalletApi#claimEmission
-  */
+
+    Buy/Sell RAM:
+    * @link WalletApi#getApproximateRamPriceByBytesAmount - set bytes from input and get approximate price in UOS to show
+    * @link WalletApi#buyRam
+    * @link WalletApi#sellRam
+   */
 })();
 
 
