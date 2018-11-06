b6136066fa84274e78b0499281ec31ed4422c98b
vladimir <mr.vladimir.ice@gmail.com>
2018-11-06 15:37:14 +0300

    stake-unstake method

--- example.js
+++ example.js
@@ -1,19 +1,20 @@
 const { WalletApi } = require('./index');
 
 (async () => {
-  const accountNameFrom = 'accregistrar';
+  // const accountNameFrom = 'accregistrar';
   const accountNameTo = 'vlad';
-  const privateKey = '5KSSv6acEyUAm4i11Mw3qxkun8t4rTC63kWs4Heh5NVkHsf6gHe';
-  const amount = '100';
-  const memo = '';
-
-
-
-
+  // const privateKey = 'private_key_here';
+  // const amount = '100';
+  // const memo = '';
   // const sendTokensResponse = await WalletApi.sendTokens(accountNameFrom, privateKey, accountNameTo, amount, memo);
   // console.dir(sendTokensResponse);
 
-  // Check unstake - special field to
+  /**
+    Staking form workflow:
+    * @link WalletApi#getCurrentNetAndCpuStakedTokens - show in form
+    * @link WalletApi#stakeOrUnstakeTokens - send data to this method
+  */
+
 
   const accountState = await WalletApi.getAccountState(accountNameTo);
   console.log(accountState);
