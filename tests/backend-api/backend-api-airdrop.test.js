ea8a8d3380285b53d5bcc24f0a08638fc768ae53
Vladimir Ice <mr.vladimir.ice@gmail.com>
2019-04-10 17:39:39 +0300

    add new method to fetch airdrops receipt

--- tests/backend-api/backend-api-airdrop.test.js
+++ tests/backend-api/backend-api-airdrop.test.js
@@ -218,8 +218,258 @@ describe('Backend API airdrop', () => {
 
       expect(actual).toMatchObject(expected);
     }, JEST_TIMEOUT);
+
+    it('Find receipt records from given external_id', async () => {
+      helper.initForStagingEnv();
+
+      const externalId = 275349;
+      const actual = await BackendApi.getAirdropsReceiptTableRowsAfterExternalId(externalId, 10);
+      const expected = getSampleAirdropsReceiptAfterExternalId();
+
+      for (let i = 0; i < 4; i += 1) {
+        expect(actual[i]).toMatchObject(expected[i]);
+      }
+    }, JEST_TIMEOUT);
   });
 });
+
+
+function getSampleAirdropsReceiptAfterExternalId() {
+  return [
+    {
+      "id": 6,
+      "external_id": 275349,
+      "airdrop_id": 29947,
+      "amount": 20001,
+      "acc_name": "petr",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 7,
+      "external_id": 312965,
+      "airdrop_id": 29947,
+      "amount": 20001,
+      "acc_name": "vlad",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 5,
+      "external_id": 470255,
+      "airdrop_id": 618018,
+      "amount": 20001,
+      "acc_name": "jane",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 9,
+      "external_id": 958064,
+      "airdrop_id": 587162,
+      "amount": 20001,
+      "acc_name": "jane",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 8,
+      "external_id": 988861,
+      "airdrop_id": 587162,
+      "amount": 20001,
+      "acc_name": "vlad",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 20,
+      "external_id": 1119045,
+      "airdrop_id": 1583784,
+      "amount": 20001,
+      "acc_name": "vlad",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 17,
+      "external_id": 1169432,
+      "airdrop_id": 3714441,
+      "amount": 20001,
+      "acc_name": "jane",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 25,
+      "external_id": 1364080,
+      "airdrop_id": 7464427,
+      "amount": 20001,
+      "acc_name": "jane",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 27,
+      "external_id": 2259812,
+      "airdrop_id": 14781593,
+      "amount": 30001,
+      "acc_name": "jane",
+      "symbol": "GHTEST"
+    },
+    {
+      "id": 30,
+      "external_id": 2807738,
+      "airdrop_id": 3044321,
+      "amount": 20001,
+      "acc_name": "summerknight",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 11,
+      "external_id": 3008790,
+      "airdrop_id": 1951188,
+      "amount": 20001,
+      "acc_name": "summerknight",
+      "symbol": "GHTEST"
+    },
+    {
+      "id": 29,
+      "external_id": 3197937,
+      "airdrop_id": 12807513,
+      "amount": 30001,
+      "acc_name": "jane",
+      "symbol": "GHTEST"
+    },
+    {
+      "id": 13,
+      "external_id": 3882236,
+      "airdrop_id": 5104204,
+      "amount": 20001,
+      "acc_name": "jane",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 31,
+      "external_id": 3991488,
+      "airdrop_id": 9373946,
+      "amount": 20001,
+      "acc_name": "summerknight",
+      "symbol": "GHTEST"
+    },
+    {
+      "id": 16,
+      "external_id": 4000336,
+      "airdrop_id": 3714441,
+      "amount": 20001,
+      "acc_name": "vlad",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 10,
+      "external_id": 4324265,
+      "airdrop_id": 4821544,
+      "amount": 20001,
+      "acc_name": "summerknight",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 32,
+      "external_id": 4464744,
+      "airdrop_id": 2603820,
+      "amount": 20001,
+      "acc_name": "vlad",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 12,
+      "external_id": 4800357,
+      "airdrop_id": 5104204,
+      "amount": 20001,
+      "acc_name": "vlad",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 19,
+      "external_id": 4936361,
+      "airdrop_id": 7826972,
+      "amount": 20001,
+      "acc_name": "summerknight",
+      "symbol": "GHTEST"
+    },
+    {
+      "id": 28,
+      "external_id": 5210779,
+      "airdrop_id": 87218862,
+      "amount": 30001,
+      "acc_name": "jane",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 33,
+      "external_id": 5546471,
+      "airdrop_id": 2603820,
+      "amount": 20001,
+      "acc_name": "jane",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 18,
+      "external_id": 5615020,
+      "airdrop_id": 8085431,
+      "amount": 20001,
+      "acc_name": "summerknight",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 24,
+      "external_id": 5646349,
+      "airdrop_id": 7464427,
+      "amount": 20001,
+      "acc_name": "vlad",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 26,
+      "external_id": 5676835,
+      "airdrop_id": 41,
+      "amount": 30001,
+      "acc_name": "jane",
+      "symbol": "GHTEST"
+    },
+    {
+      "id": 21,
+      "external_id": 6225753,
+      "airdrop_id": 1583784,
+      "amount": 20001,
+      "acc_name": "jane",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 14,
+      "external_id": 6309138,
+      "airdrop_id": 3345720,
+      "amount": 20001,
+      "acc_name": "summerknight",
+      "symbol": "UOSTEST"
+    },
+    {
+      "id": 23,
+      "external_id": 9299101,
+      "airdrop_id": 8072423,
+      "amount": 20001,
+      "acc_name": "summerknight",
+      "symbol": "GHTEST"
+    },
+    {
+      "id": 15,
+      "external_id": 9392554,
+      "airdrop_id": 2260179,
+      "amount": 20001,
+      "acc_name": "summerknight",
+      "symbol": "GHTEST"
+    },
+    {
+      "id": 22,
+      "external_id": 9937582,
+      "airdrop_id": 1882053,
+      "amount": 20001,
+      "acc_name": "summerknight",
+      "symbol": "UOSTEST"
+    }
+  ];
+}
 
 function getSampleAirdropsReceiptTableRows() {
   return [
