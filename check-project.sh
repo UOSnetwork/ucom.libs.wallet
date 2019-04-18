a1dbaf9c5e9ff54ecf5203e7397261da7c2e5927
Vladimir Ice <mr.vladimir.ice@gmail.com>
2019-04-18 15:26:51 +0300

    check-project is improved

--- check-project.sh
+++ check-project.sh
@@ -2,10 +2,18 @@
 
 echo "Let's check project"
 gitOutput="$(git status --porcelain)"
+npmOutdated="$(npm outdated)"
 
 if [[ -z "$gitOutput" ]]; then
 echo "Working directory clean"
 else
     echo ERROR: Git status - there are changes. Please commit them. ${gitOutput}. 1>&2
+    exit 1 # terminate and indicate error
+fi
+
+if [[ -z "$npmOutdated" ]]; then
+echo "All packages are up to date"
+else
+    echo ERROR: There are a couple of outdated packages. 1>&2
     exit 1 # terminate and indicate error
 fi
