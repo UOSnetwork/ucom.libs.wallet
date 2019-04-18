1206e664d02e95f2dfddce90216d4f09b8d02cb4
Vladimir Ice <mr.vladimir.ice@gmail.com>
2019-04-18 15:46:51 +0300

    add js extensions inside index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 /* eslint-disable global-require */
 module.exports = {
-  WalletApi: require('./lib/wallet-api'),
-  SocialApi: require('./lib/social-api'),
+  WalletApi: require('./lib/wallet-api.js'),
+  SocialApi: require('./lib/social-api.js'),
 
-  BackendApi: require('./lib/backend-api'),
+  BackendApi: require('./lib/backend-api.js'),
 
-  TransactionSender: require('./lib/transaction-sender'),
-  UosAccountsPropertiesApi: require('./lib/uos-accounts-properties/uos-accounts-properties-api'),
-  EosClient: require('./lib/eos-client'),
-  ConfigService: require('./config/config-service'),
+  TransactionSender: require('./lib/transaction-sender.js'),
+  UosAccountsPropertiesApi: require('./lib/uos-accounts-properties/uos-accounts-properties-api.js'),
+  EosClient: require('./lib/eos-client.js'),
+  ConfigService: require('./config/config-service.js'),
   Dictionary: {
-    BlockchainTrTraces: require('./lib/dictionary/blockchain-tr-traces-dictionary'),
+    BlockchainTrTraces: require('./lib/dictionary/blockchain-tr-traces-dictionary.js'),
   },
 };
